b446365307401af109df202bef84753366d69de6
Ileana Aguilar <ileanaap9@gmail.com>
2022-08-02 12:09:50 -0700

    Final code

--- code-challenge.ts
+++ code-challenge.ts
@@ -1,7 +1,19 @@
-type BEM<B extends string, E extends string[], M extends string[]> =
-    E extends []
-        ? M extends []
-            ? B : `${B}--${M[number]}`
-            : M extends []
-              ? `${B}__${E[number]}`
-              : `${B}__${E[number]}--${M[number]}`
+//Ileana Aguilar -- Typescript Challenge
+
+function BEM<B extends string, E extends string[], M extends string[]>(block: B, element: E, modifier: M) {
+  let result;
+
+  //block__element
+  if (element.length > 0 && modifier.length === 0) {
+    result = `${block}__${element}`;
+    return console.log(result);
+  } else if (element.length === 0 && modifier.length > 0) { // block--modifier
+    result = `${block}--${modifier}`;
+    return console.log(result);
+  } else if (element.length > 0 && modifier.length > 0) { //block__element--modifier
+    result = `${block}__${element}--${modifier}`;
+    return console.log(result);
+  }
+}
+
+const bem = BEM<string, string[], string[]>("block", ["element"], ["modifier"]);
